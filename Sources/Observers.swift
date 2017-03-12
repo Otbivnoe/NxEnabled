ff9657864e64df42c95962bb64ffe75f6ddd5b09
ISosnovik <sosnovikivan@gmail.com>
2017-03-12 14:54:44 +0300

    refactor

--- Sources/Observers.swift
+++ Sources/Observers.swift
@@ -10,12 +10,8 @@ import UIKit.UIControl
 
 typealias EnabledHandler = (Bool) -> Void
 
-public typealias ConfigurationHandler1 = (String) -> Bool
-public typealias ConfigurationHandler2 = (String, String) -> Bool
-public typealias ConfigurationHandler3 = (String, String, String) -> Bool
-public typealias ConfigurationHandler4 = (String, String, String, String) -> Bool
-public typealias ConfigurationHandler5 = (String, String, String, String, String) -> Bool
-public typealias ConfigurationHandler6 = (String, String, String, String, String, String) -> Bool
+public typealias ConfigurationHandler = ([String]) -> Bool
+
 
 // MARK: - NUIObserver
 
@@ -32,7 +28,7 @@ class NUIObserver: NSObject {
     
     //TODO: Refactore later, when apple fix it https://bugs.swift.org/browse/SR-3849
     
-    init(textableValues: [NSObject], enabledHandler: @escaping EnabledHandler) {
+    init(textableValues: [NSObjectProtocol], enabledHandler: @escaping EnabledHandler) {
         self.enabledHandler = enabledHandler
         self.textableValues = NSPointerArray(options: .weakMemory)
         
@@ -81,13 +77,7 @@ class NUIObserver: NSObject {
     fileprivate func configureEnabling() {
         preconditionFailure("This method must be overridden")
     }
-    
-    fileprivate func text(at index: Int) -> String {
-        let tuple = textableValueTuple(by: textableValues.allObjects[index])
-        let value = tuple.value.value(forKey: tuple.key) as! String?
-        return value ?? ""
-    }
-    
+        
     private func textableValueTuple(by value: Any) -> (value:NSObject, key:String) {
         let textableValue = value as! NSObject
         let key = (textableValue as! Textable).textKey
@@ -95,14 +85,13 @@ class NUIObserver: NSObject {
     }
 }
 
-// MARK: - NUIObserver1
+// MARK: - NUIConfigurableObserver
 
-final class NUIObserver1: NUIObserver {
+final class NUIConfigurableObserver: NUIObserver {
     
-    typealias ConfigurationHandler = ConfigurationHandler1
     private var configurationHandler: ConfigurationHandler
     
-    init(textableValues: [NSObject],
+    init(textableValues: [NSObjectProtocol],
          configurationHandler: @escaping ConfigurationHandler,
          enabledHandler: @escaping EnabledHandler) {
         
@@ -111,127 +100,11 @@ final class NUIObserver1: NUIObserver {
     }
 
     fileprivate override func configureEnabling() {
-        let text = self.text(at: 0)
-        enabledHandler(configurationHandler(text))
-    }
-}
-
-// MARK: - NUIObserver2
-
-final class NUIObserver2: NUIObserver {
-    
-    typealias ConfigurationHandler = ConfigurationHandler2
-    private var configurationHandler: ConfigurationHandler
-    
-    init(textableValues: [NSObject],
-         configurationHandler: @escaping ConfigurationHandler,
-         enabledHandler: @escaping EnabledHandler) {
-        
-        self.configurationHandler = configurationHandler
-        super.init(textableValues: textableValues, enabledHandler: enabledHandler)
-    }
-    
-    fileprivate override func configureEnabling() {
-        let text0 = self.text(at: 0)
-        let text1 = self.text(at: 1)
-        enabledHandler(configurationHandler(text0, text1))
-    }
-}
-
-// MARK: - NUIObserver3
-
-final class NUIObserver3: NUIObserver {
-    
-    typealias ConfigurationHandler = ConfigurationHandler3
-    private var configurationHandler: ConfigurationHandler
-    
-    init(textableValues: [NSObject],
-         configurationHandler: @escaping ConfigurationHandler,
-         enabledHandler: @escaping EnabledHandler) {
-        
-        self.configurationHandler = configurationHandler
-        super.init(textableValues: textableValues, enabledHandler: enabledHandler)
-    }
-
-    fileprivate override func configureEnabling() {
-        let text0 = self.text(at: 0)
-        let text1 = self.text(at: 1)
-        let text2 = self.text(at: 2)
-        enabledHandler(configurationHandler(text0, text1, text2))
-    }
-}
-
-// MARK: - NUIObserver4
-
-final class NUIObserver4: NUIObserver {
-    
-    typealias ConfigurationHandler = ConfigurationHandler4
-    private var configurationHandler: ConfigurationHandler
-    
-    init(textableValues: [NSObject],
-         configurationHandler: @escaping ConfigurationHandler,
-         enabledHandler: @escaping EnabledHandler) {
-        
-        self.configurationHandler = configurationHandler
-        super.init(textableValues: textableValues, enabledHandler: enabledHandler)
-    }
-    
-    fileprivate override func configureEnabling() {
-        let text0 = self.text(at: 0)
-        let text1 = self.text(at: 1)
-        let text2 = self.text(at: 2)
-        let text3 = self.text(at: 3)
-        enabledHandler(configurationHandler(text0, text1, text2, text3))
-    }
-}
-
-// MARK: - NUIObserver5
-
-final class NUIObserver5: NUIObserver {
-    
-    typealias ConfigurationHandler = ConfigurationHandler5
-    private var configurationHandler: ConfigurationHandler
-    
-    init(textableValues: [NSObject],
-         configurationHandler: @escaping ConfigurationHandler,
-         enabledHandler: @escaping EnabledHandler) {
-        
-        self.configurationHandler = configurationHandler
-        super.init(textableValues: textableValues, enabledHandler: enabledHandler)
-    }
-    
-    fileprivate override func configureEnabling() {
-        let text0 = self.text(at: 0)
-        let text1 = self.text(at: 1)
-        let text2 = self.text(at: 2)
-        let text3 = self.text(at: 3)
-        let text4 = self.text(at: 4)
-        enabledHandler(configurationHandler(text0, text1, text2, text3, text4))
-    }
-}
-
-// MARK: - NUIObserver6
-
-final class NUIObserver6: NUIObserver {
-    
-    typealias ConfigurationHandler = ConfigurationHandler6
-    private var configurationHandler: ConfigurationHandler
-    
-    init(textableValues: [NSObject],
-         configurationHandler: @escaping ConfigurationHandler,
-         enabledHandler: @escaping EnabledHandler) {
-        
-        self.configurationHandler = configurationHandler
-        super.init(textableValues: textableValues, enabledHandler: enabledHandler)
-    }
-
-    fileprivate override func configureEnabling() {
-        let text0 = self.text(at: 0)
-        let text1 = self.text(at: 1)
-        let text2 = self.text(at: 2)
-        let text3 = self.text(at: 3)
-        let text4 = self.text(at: 4)
-        let text5 = self.text(at: 5)
-        enabledHandler(configurationHandler(text0, text1, text2, text3, text4, text5))
+        let texts = self.textableValues.allObjects.map { (t) -> String in
+            let textableValue = t as! Textable
+            let objectValue = t as! NSObject
+            return objectValue.value(forKey: textableValue.textKey) as? String ?? ""
+        }
+        enabledHandler(configurationHandler(texts))
     }
 }
